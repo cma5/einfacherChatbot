6d3038be68b59669df4e6e27e87693e228340856
gsocher <gudrun.socher@hm.edu>
2022-10-21 17:29:36 +0200

    upload project

---
+++ actions/actions.py
@@ -0,0 +1,28 @@
+# This files contains your custom actions which can be used to run
+# custom Python code.
+#
+# See this guide on how to implement these action:
+# https://rasa.com/docs/rasa/custom-actions
+
+
+# This is a simple example for a custom action which utters "Hello World!"
+
+from typing import Any, Text, Dict, List
+
+from rasa_sdk import Action, Tracker
+from rasa_sdk.executor import CollectingDispatcher
+
+class ActionUserName(Action):
+
+     def name(self) -> Text:
+         return "action_get_name"
+
+     def run(self, dispatcher, tracker, domain):
+        username = tracker.get_slot("username")
+        if not username :
+            dispatcher.utter_message(" Du hast mir Deinen Namen nicht gesagt.")
+        else:
+            dispatcher.utter_message(' Du bist {}'.format(username))
+
+
+        return []
