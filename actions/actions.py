189815bc3231a5837a4fc3c0984c032da849ed86
Michael Eggers <mseggers@protonmail.ch>
2022-10-27 09:29:32 +0200

    add intent that challenges bot to remember username. add corresponding rule. add dummy action that could potentially save username into DB

--- actions/actions.py
+++ actions/actions.py
@@ -8,25 +8,27 @@
 # This is a simple example for a custom action which utters "Hello World!"
 
 from typing import Any, Text, Dict, List
+from pyparsing import nestedExpr
 
 from rasa_sdk import Action, Tracker
 from rasa_sdk.executor import CollectingDispatcher
 
 
-
-
+# NOTE(Michael): We could use this action to store the name in
+#                the TrackerStore (in memory database) or a persitent DB
+#                such as MySQL. But we need to store a key-value pair 
+#                to identify the user by id eg. (user_id, slotvalue)
 class ActionStoreUserName(Action):
 
      def name(self) -> Text:
          return "action_store_name"
-
-
+         
      def run(self, dispatcher, tracker, domain):
         username = tracker.get_slot("username")
-        sender_ids = tracker.keys()
-        print(sender_ids)
+        print("Sender ID: ", tracker.sender_id)
 
         return []
+
 
 class ActionUserName(Action):
 
@@ -39,7 +41,6 @@ class ActionUserName(Action):
             dispatcher.utter_message(" Du hast mir Deinen Namen nicht gesagt.")
         else:
             dispatcher.utter_message(' Du bist {}'.format(username))
-
 
         return []
 
